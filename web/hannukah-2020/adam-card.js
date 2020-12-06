0a2863a9cd6eb8aa05cc1891bd3947a32ce63382
Daniel Winterstein <daniel@good-loop.com>
2020-12-06 16:28:49 +0000

    done
    Former-commit-id: ccf716518a3df927d36534d758feb2f80de01638

--- web/hannukah-2020/adam-card.js
+++ web/hannukah-2020/adam-card.js
@@ -7,7 +7,7 @@ let mywords = document.getElementById("mywords");
 let clickme = document.getElementById("clickme");
 
 clickme.onclick = function () {
-	alert("Hello :)");
+	// alert("Hello :)");
 	// swim right (which we do by "move increasingly further away from the left of the box")
 	animate(4, function ({ fraction, dt }) {
 		myimg.style.left = (-129 + (500 * fraction)) + "px";
